00e87fa489c5be54411325df5d4d3bb9785fe5d4
Harikrishnakarthikeyan <122782884+Harikrishnakarthikeyan@users.noreply.github.com>
2025-08-05 18:48:36 +0530

    Time: 600 ms (7.52%) | Memory: 0B (100.00%) - LeetSync

--- 3782-find-valid-emails/find-valid-emails.sql
+++ 3782-find-valid-emails/find-valid-emails.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT *
-FROM Users
-WHERE REGEXP_LIKE(email, '^[a-zA-Z0-9_]+@[a-zA-Z]+[.]+[com]') and right(email,4)='.com';
+SELECT * FROM Users
+WHERE REGEXP_LIKE(email, '^[a-zA-Z0-9_]+@[a-zA-Z]+[.]+[com]') 
+and right(email,4)='.com';
