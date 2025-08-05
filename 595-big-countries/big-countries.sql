10d89a56e546344d3145bcdf956ef9e231ff9c55
Harikrishnakarthikeyan <122782884+Harikrishnakarthikeyan@users.noreply.github.com>
2025-08-05 19:10:42 +0530

    Time: 420 ms (8.75%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,1 +1,2 @@
-select name, population , area from world where population >= 25000000 or area >=3000000
+select w.name, w.population, w.area from World w 
+where w.area>=3000000 or w.population >=25000000;
