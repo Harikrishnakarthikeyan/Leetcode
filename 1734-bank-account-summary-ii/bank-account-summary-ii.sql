115ab03374cd4ac00006e1db7d5367c8519150ae
Harikrishnakarthikeyan <122782884+Harikrishnakarthikeyan@users.noreply.github.com>
2025-08-05 13:25:19 +0530

    Time: 730 ms (85.04%) | Memory: 0B (100.00%) - LeetSync

--- 1734-bank-account-summary-ii/bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -1,1 +1,2 @@
-select u.name NAME,sum(t.amount) BALANCE from Users as u join Transactions as t on u.account = t.account group by t.account having BALANCE  > 10000;
+select u.name NAME,sum(t.amount) BALANCE from Users as u join Transactions as t
+ on u.account = t.account group by t.account having BALANCE  > 10000;
