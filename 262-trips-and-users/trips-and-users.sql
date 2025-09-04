4bda2b29fa4c9cefc8816b15e0af870696d37d78
Harikrishnakarthikeyan <122782884+Harikrishnakarthikeyan@users.noreply.github.com>
2025-09-04 10:53:42 +0530

    Time: 580 ms (35.41%) | Memory: 0B (100.00%) - LeetSync

---
+++ 262-trips-and-users/trips-and-users.sql
@@ -0,0 +1,18 @@
+
+
+WITH cte AS (
+    SELECT 
+        request_at AS 'Day',
+        ROUND(SUM(IF(status != 'completed', 1, 0)) / COUNT(status), 2) AS 'Cancellation Rate'
+    FROM Trips
+    WHERE request_at BETWEEN '2013-10-01' AND '2013-10-03'
+      AND client_id NOT IN (
+          SELECT users_id FROM users WHERE banned = 'Yes'
+      )
+      AND driver_id NOT IN (
+          SELECT users_id FROM users WHERE banned = 'Yes'
+      )
+    GROUP BY request_at
+)
+
+SELECT * FROM cte;
