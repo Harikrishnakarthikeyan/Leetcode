ac9641d878c88c06ec8ee3bcd159238cbc36c75a
Harikrishnakarthikeyan <122782884+Harikrishnakarthikeyan@users.noreply.github.com>
2025-09-23 18:22:04 +0530

    Time: 290 ms (77.65%) | Memory: 0B (100.00%) - LeetSync

--- 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
+++ 3848-analyze-subscription-conversion/analyze-subscription-conversion.sql
@@ -8,16 +8,5 @@ where u.activity_type IN ('free_trial','paid')
 group by u.user_id
 HAVING COUNT(DISTINCT u.activity_type) = 2
 
-    /*select distinct 
-    from UserActivity
-    where activity_type = 'free_trial'
-    group by user_id /*
-
-paid as (
-    select distinct round(avg(activity_duration),2) paid_avg_duration 
-    from UserActivity
-    where activity_type = 'paid' 
-    group by user_id
-)*/
 
 
