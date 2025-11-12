13b5e6677552a215ccdd257ebe2299b47d573207
Harikrishnakarthikeyan <122782884+Harikrishnakarthikeyan@users.noreply.github.com>
2025-11-12 09:53:18 +0530

    Time: 241 ms (88.11%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,9 +1,11 @@
 # Write your MySQL query statement below
-with cte as (
-select max(salary) as SecondHighestSalary
-from employee
-where salary < (select max(salary) as SecondHighestSalary
-from employee)
-
-)
-select * from cte;
+with cte as(
+select distinct Salary  as SecondHighestSalary
+from Employee
+order by Salary  desc limit 1,1)
+Select 
+Case 
+when count(SecondHighestSalary) > 0 then SecondHighestSalary
+else Null
+end as SecondHighestSalary
+from cte;
