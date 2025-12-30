6a2937886c0b00ded5e68ad3d1972db8c3c0a0ef
Harikrishnakarthikeyan <122782884+Harikrishnakarthikeyan@users.noreply.github.com>
2025-12-30 13:35:10 +0530

    Time: 3 ms (67.93%) | Memory: 48 MB (17.20%) - LeetSync

--- 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -1,10 +1,10 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
         List<Integer> li=new ArrayList<>();
-        int carray=0;
-        int i=num.length-1;
+    
+        int i=num.length-1,sum=0;
         while(i >=0 || k>0){
-            int sum=carray;
+             
             if(i>=0){
                 sum+=num[i];
                 i--;
@@ -14,10 +14,10 @@ class Solution {
                 k/=10;
             }
             li.add(sum%10);
-            carray=sum/10;
+            sum/=10;
         }
-        if (carray>0){
-            li.add(carray);
+        if (sum>0){
+            li.add(sum);
         }
 Collections.reverse(li);
         return li;
