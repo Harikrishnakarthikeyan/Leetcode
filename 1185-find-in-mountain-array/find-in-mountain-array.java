7d2ed9c89f6507dcbd6105e316279ccce5aea5ae
Harikrishnakarthikeyan <122782884+Harikrishnakarthikeyan@users.noreply.github.com>
2026-02-25 09:29:37 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (97.57%) - LeetSync

--- 1185-find-in-mountain-array/find-in-mountain-array.java
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -11,46 +11,47 @@ class Solution {
     public int findInMountainArray(int target, MountainArray mountainArr) {
         int start=0;
         int end=mountainArr.length()-1;
-        
-        while(start<end){
-            int mid=start+(end-start)/2;
-            if(mountainArr.get(mid)>mountainArr.get(mid+1))
-            end=mid;
-            else
-            start=mid+1;
-        }
-        int ans=binarySearch(mountainArr,target,0,end);
+        int peek=findPeek(mountainArr,start,end);
+        int ans=binarySearch(start,peek,mountainArr,target,true);
         if(ans==-1)
-        ans=binarySearch(mountainArr,target,end,mountainArr.length()-1);
+         ans=binarySearch(peek,end,mountainArr,target,false);
         return ans;
     }
-    int binarySearch(MountainArray mountainArr,int target,int start,int end){
-        boolean isAsc= mountainArr.get(start)<mountainArr.get(end);
+    public int findPeek(MountainArray mountainArr,int start,int end){
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(mountainArr.get(mid)>mountainArr.get(mid+1)){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+    public int binarySearch(int start,int end,MountainArray mountainArr,int target,boolean isAsc){
         while(start<=end){
             int mid=start+(end-start)/2;
             if(mountainArr.get(mid)==target){
                 return mid;
-            }
-            else{
-            if(isAsc){
-                if(mountainArr.get(mid)>target){
-                end=mid-1;
-            }
-            else {
-                start=mid+1;
-            }
-
-
-            }
-            else{
-            if(mountainArr.get(mid)<target){
-                end=mid-1;
-            }
-            else {
-                start=mid+1;
+            }else{
+                if(isAsc){
+                    if(mountainArr.get(mid)>target){
+                        end=mid-1;
+                    }
+                    else{
+                        start=mid+1;
+                    }
+                }
+                else{
+                    if(mountainArr.get(mid)<target){
+                        end=mid-1;
+                    }
+                    else{
+                        start=mid+1;
+                    }
                 }
             }
-        }
         }
         return -1;
     }
